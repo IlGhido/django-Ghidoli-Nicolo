6560b716e75fa8d1fc8bd7a4a919abfb3c9c2901
IlGhido <nicolo.ghidoli@gmail.com>
2021-11-04 10:59:44 +0100

    livello4

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,6 +3,7 @@ from prima_app.views import homepage
 from prima_app.views import welcome
 from prima_app.views import lista
 from prima_app.views import chi_siamo
+from prima_app.views import variabili
 
 app_name="prima_app"
 urlpatterns=[
@@ -10,4 +11,5 @@ urlpatterns=[
     path('welcome', welcome, name='welcome'),
     path("lista",lista,name="lista"),
     path("chi_siamo",chi_siamo,name="chi_siamo"),
+    path("variabili",variabili,name="variabili"),
 ]
