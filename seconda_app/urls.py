8555a6585d16e8c290d1f6910fe9b78de1dece2e
IlGhido <nicolo.ghidoli@gmail.com>
2021-11-11 10:15:50 +0100

    livello7

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls.conf import path
 
 from .views import es_if
+from .views import if_else_elif
 
 
 app_name="seconda_app"
 
 urlpatterns = [
-    path("es_if",es_if,name="es_if")
+    path("es_if",es_if,name="es_if"),
+    path("if_else_elif",if_else_elif,name="if_else_elif"),
 ]
