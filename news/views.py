1319f1a129435ab1c7cea7a75fd3bb8df502f9f4
IlGhido <nicolo.ghidoli@gmail.com>
2021-12-02 12:48:38 +0100

    livello 11_4

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 def home(request):
     articoli = Articolo.objects.all()
@@ -18,4 +19,27 @@ class ArticoloDetailViewCB(DetailView):
     model=Articolo
     template_name="articolo_detail.html"
 
+class ArticoloListView(ListView):
+    model=Articolo
+    template_name="lista_articoli.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["articoli"]=Articolo.objects.all()
+        return context
+
+class GiornalistaDetailViewCB(DetailView):
+    model=Giornalista
+    template_name="giornalista_detail.html"
+
+class GiornalistaListView(ListView):
+    model=Giornalista
+    template_name="lista_giornalisti.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["giornalisti"]=Giornalista.objects.all()
+        return context
+
+        
 # Create your views here.
