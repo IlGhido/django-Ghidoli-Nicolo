89bb60e5a20ea391d80f2af0648955f0501382fe
IlGhido <nicolo.ghidoli@gmail.com>
2021-12-02 10:59:42 +0100

    livello 11_1

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, ArticoloDetailViewCB
 
 app_name="news"
 
 urlpatterns = [
     path('', home, name="homeview"),
+    path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail")
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
+from django.views.generic.detail import DetailView
 
 def home(request):
     articoli = Articolo.objects.all()
@@ -8,4 +8,14 @@ def home(request):
     context = {"articoli": articoli, "giornalisti": giornalisti}
     print(context)
     return render(request, "homepage.html", context)
+
+def articoloDetailView(request, pk):
+    articolo=get_object_or_404(Articolo, pk=pk)
+    context={"articolo":articolo}
+    return render(request, "articolo_detail.html",context)
+
+class ArticoloDetailViewCB(DetailView):
+    model=Articolo
+    template_name="articolo_detail.html"
+
 # Create your views here.
