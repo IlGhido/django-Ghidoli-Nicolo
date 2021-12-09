1a5a8de7a995fc11d713ba7d14462841590dbc0a
IlGhido <nicolo.ghidoli@gmail.com>
2021-12-09 10:27:30 +0100

    livello 12_6

--- news/models.py
+++ news/models.py
@@ -5,8 +5,12 @@ class Giornalista(models.Model):
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.nome + " " + self.cognome
+    #def __str__(self):
+        #return self.nome + " " + self.cognome
+
+    class Meta:
+        verbose_name = "Giornalista"
+        verbose_name_plural = "Giornalista"
     
 class Articolo(models.Model):
     """ modello generico di un articolo """
@@ -14,7 +18,11 @@ class Articolo(models.Model):
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
 
-    def __str__(self):
-        return self.titolo
+    #def __str__(self):
+        #return self.titolo
+    
+    class Meta:
+        verbose_name = "Articolo"
+        verbose_name_plural = "Articolo"
 
 # Create your models here.
