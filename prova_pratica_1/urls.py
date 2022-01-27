71828cd5798695b32fa6d62177cd1dc9b158b876
IlGhido <nicolo.ghidoli@gmail.com>
2022-01-27 11:18:37 +0100

    view_d

--- prova_pratica_1/urls.py
+++ prova_pratica_1/urls.py
@@ -1,4 +1,5 @@
 from django.urls.conf import path
+from .views import view_d
 
 from .views import view_b
 from .views import view_c
@@ -8,5 +9,6 @@ app_name="prova_pratica_1"
 
 urlpatterns = [
     path("view_b",view_b,name="view_b"),
-    path("view_c",view_c,name="view_c")
+    path("view_c",view_c,name="view_c"),
+    path('view_d',view_d,name="view_d")
 ]
