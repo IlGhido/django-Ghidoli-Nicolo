c2d145939f544e6708c7639efca122ed0abcdb0e
IlGhido <nicolo.ghidoli@gmail.com>
2022-01-27 12:52:50 +0100

    view_e non finita (popolati i db)

--- prova_pratica_1/admin.py
+++ prova_pratica_1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Materia, Studente, Voto, Assenza
+
+admin.site.register(Materia)
+admin.site.register(Studente)
+admin.site.register(Voto)
+admin.site.register(Assenza)
 
 # Register your models here.
--- prova_pratica_1/models.py
+++ prova_pratica_1/models.py
@@ -4,4 +4,34 @@ class Materia(models.Model):
     nome = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.materia
+        return self.nome
+
+    class Meta:
+        verbose_name = "Materia"
+        verbose_name_plural = "Materia"
+
+class Studente(models.Model):
+    nome = models.CharField(max_length=20)
+    cognome = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Studente"
+        verbose_name_plural = "Studenti"
+
+class Voto(models.Model):
+    numero = models.FloatField()
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="voti")
+    studente = models.ForeignKey(Studente, on_delete=models.CASCADE, related_name="voti")
+
+    class Meta:
+        verbose_name = "Voto"
+        verbose_name_plural = "Voti"
+
+class Assenza(models.Model):
+    oreAssenza = models.BigIntegerField()
+    studente = models.ForeignKey(Studente, on_delete=models.CASCADE, related_name="assenze")
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="assenze")
+
+    class Meta:
+        verbose_name = "Assenza"
+        verbose_name_plural = "Assenze"
